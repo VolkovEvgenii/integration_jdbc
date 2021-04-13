bf525e3134ef9bcf0bd00ad0542664d88ce1bf8d
evgeniy volkov <volkov.e.n@gmail.com>
2021-04-13 17:26:37 +0300

    init

---
+++ src/test/java/ru/volkov/integration/jdbc/JdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.volkov.integration.jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
